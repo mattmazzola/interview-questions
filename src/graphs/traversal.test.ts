2b1ab32457aae7cff4d6e96f49858f81079592ad
Matt Mazzola <mattmazzola@live.com>
2017-05-11 22:37:28 -0700

    Add iterive version of DFS and BFS

--- src/graphs/traversal.test.ts
+++ src/graphs/traversal.test.ts
@@ -1,47 +1,74 @@
-import { INode, depthFirstTraveral } from './traversal'
+import { INode, depthFirstTraveral, depthFirstTraveralIterative, breadthFirstTraversal } from './traversal'
 
-describe('depthFirstTraveral', () => {
-    test('should return order of nodes using depth first', () => {
-        // Arrange
-        const root: INode<number> = {
-            value: 1,
+const root: INode<number> = {
+    value: 1,
+    nodes: [
+        {
+            value: 2,
             nodes: [
                 {
-                    value: 2,
-                    nodes: [
-                        {
-                            value: 3,
-                            nodes: []
-                        },
-                        {
-                            value: 4,
-                            nodes: []
-                        }
-                    ]
+                    value: 3,
+                    nodes: []
                 },
                 {
-                    value: 5,
+                    value: 4,
+                    nodes: []
+                }
+            ]
+        },
+        {
+            value: 5,
+            nodes: [
+                {
+                    value: 6,
                     nodes: [
                         {
-                            value: 6,
-                            nodes: [
-                                {
-                                    value: 7,
-                                    nodes: []
-                                }
-                            ]
+                            value: 7,
+                            nodes: []
                         }
                     ]
                 }
             ]
         }
+    ]
+}
 
-        const expectedValues = [1,2,3,4,5,6,7]
+const expectedDepthFirstValues = [1,2,3,4,5,6,7]
+const expectedBreadthFirstValues = [1,2,5,3,4,6,7]
+
+describe('depthFirstTraveral', () => {
+
+    test('should return order of nodes using depth first', () => {
+        // Arrange
 
         // Act
         const depthFirstValues = depthFirstTraveral(root)
 
         // Assert
-        expect(depthFirstValues).toEqual(expectedValues)
+        expect(depthFirstValues).toEqual(expectedDepthFirstValues)
+    })
+})
+
+describe('depthFirstTraveralIterative', () => {
+    test('should return order of nodes using depth first', () => {
+        // Arrange
+
+        // Act
+        const depthFirstValues = depthFirstTraveralIterative(root)
+
+        // Assert
+        expect(depthFirstValues).toEqual(expectedDepthFirstValues)
+    })
+})
+
+describe('breadthFirstTraversal', () => {
+    test('should return order of node value in breadth first order', () => {
+        // Arrange
+
+        // Act
+        const breadthFirstValues = breadthFirstTraversal(root)
+
+        // Assert
+        expect(breadthFirstValues).toEqual(expectedBreadthFirstValues)
     })
 })
--- src/graphs/traversal.ts
+++ src/graphs/traversal.ts
@@ -3,10 +3,67 @@ export interface INode<T> {
     nodes: INode<T>[]
 }
 
+export interface IVisited<T> {
+    item: T
+    visited: boolean
+}
+
+class Visited<T> implements IVisited<T> {
+    item: T
+    parent: T
+    visited = false
+
+    constructor(item: T) {
+        this.item = item
+    }
+}
+
 export const depthFirstTraveral = <T>(root: INode<T>): T[] => {
     if (root.nodes.length === 0) {
         return [root.value]
     }
 
     return [root.value, ...root.nodes.map(depthFirstTraveral).reduce((a,b) => a.concat(b))]
+}
+
+export const depthFirstTraveralIterative = <T>(root: INode<T>): T[] => {
+    const nodes: IVisited<INode<T>>[] = []
+    const values: T[] = []
+
+    nodes.push(new Visited(root))
+
+    while (nodes.length > 0) {
+        let n1 = nodes.pop()!
+        
+        if (n1.visited == false) {
+            n1.visited = true
+            values.push(n1.item.value)
+            reverse(n1.item.nodes).forEach(n => nodes.push(new Visited(n)))
+        }
+    }
+
+    return values
+}
+
+const reverse = <T>(xs: T[]): T[] => {
+    return [...xs].reverse()
+}
+
+export const breadthFirstTraversal = <T>(root: INode<T>): T[] => {
+    const nodes: IVisited<INode<T>>[] = []
+    const values: T[] = []
+
+    nodes.unshift(new Visited(root))
+
+    while (nodes.length > 0) {
+        let n1 = nodes.pop()!
+        
+        if (n1.visited == false) {
+            n1.visited = true
+            values.push(n1.item.value)
+            n1.item.nodes.forEach(n => nodes.unshift(new Visited(n)))
+        }
+    }
+
+    return values
 }
