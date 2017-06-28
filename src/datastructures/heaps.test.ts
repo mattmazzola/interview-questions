98648d5720be7f1f4a5148b5e7c1dd20269f17e1
Matt Mazzola <mattmazzola@live.com>
2017-06-27 21:21:08 -0700

    Add priority queues implemented using array or heap

--- src/datastructures/heaps.test.ts
+++ src/datastructures/heaps.test.ts
@@ -1,6 +1,28 @@
 import * as heap from './heaps'
 
 describe('heap', () => {
+    test('sort items by using max heap', () => {
+        // Arrange
+        const input = [4,7,5,8,9,0,2,1,52,3,40]
+        const expected = [52,40,9,8,7,5,4,3,2,1,0]
+
+        // Act
+        const h1 = new heap.Heap()
+        input.forEach(x => h1.insert(x))
+
+        const sortedItems = []
+        let max = h1.remove()
+        while (max !== undefined) {
+            sortedItems.push(max)
+            max = h1.remove()
+        }
+
+        // Arrange
+        expect(sortedItems).toEqual(expected)
+    })
+})
+
+describe('priorityqueue', () => {
     test('test opeations of priority queue implemented using an array', () => {
         // Arrange
         const priorityQueue = new heap.PriorityQueueArray()
@@ -12,6 +34,31 @@ describe('heap', () => {
         const max1 = priorityQueue.removeMax()
 
         priorityQueue.empty()
+        priorityQueue.insert(123124)
+        priorityQueue.insert(-123124)
+        priorityQueue.insert(0)
+        priorityQueue.insert(10)
+        const max2 = priorityQueue.removeMax()
+
+        priorityQueue.insert(98)
+        const max3 = priorityQueue.removeMax()
+
+        // Assert
+        expect(max1).toBe(10)
+        expect(max2).toBe(123124)
+        expect(max3).toBe(98)
+    })
+
+    test('test opeations of priority queue implemented using a heap', () => {
+        // Arrange
+        const priorityQueue = new heap.PriorityQueue()
+
+        // Act
+        priorityQueue.insert(1)
+        priorityQueue.insert(10)
+        priorityQueue.insert(6)
+        const max1 = priorityQueue.removeMax()
+
         priorityQueue.insert(123124)
         priorityQueue.insert(-123124)
         priorityQueue.insert(0)
--- src/datastructures/heaps.ts
+++ src/datastructures/heaps.ts
@@ -1,3 +1,73 @@
+/**
+ * Heap data structure using array as backing data
+ */
+export class Heap {
+    items: number[] = [Number.MIN_VALUE]
+
+    insert(x: number) {
+        let newItemIndex = this.items.push(x) - 1
+
+        // Move item up tree until it is not greater than its parent or reaches the top
+        let parentIndex = Math.floor(newItemIndex/2)
+        while (parentIndex > 0) {
+            if (this.items[newItemIndex] <= this.items[parentIndex]) {
+                break
+            }
+
+            const temp = this.items[parentIndex]
+            this.items[parentIndex] = this.items[newItemIndex]
+            this.items[newItemIndex] = temp
+            newItemIndex = parentIndex
+            parentIndex = Math.floor(parentIndex/2)
+        }
+    }
+
+    remove(): number | undefined {
+        if (this.items.length === 1) {
+            return undefined
+        }
+
+        if (this.items.length === 2) {
+            return this.items.pop()!
+        }
+
+        // Save current max to return
+        const root = this.items[1]
+        
+        // Set last item as root and move it down tree until it is not less then a child or reachs leaf
+        this.items[1] = this.items.pop()!
+        let itemIndex = 1
+        while (itemIndex < this.items.length) {
+            const leftIndex = itemIndex * 2
+            const rightIndex = itemIndex * 2 + 1
+
+            let left = (leftIndex < this.items.length) ? this.items[leftIndex] : null
+            let right = (rightIndex < this.items.length) ? this.items[rightIndex] : null
+
+            // If one of the children is larger, swap the largest child with the the parent (item)
+            let indexOfLargestItem = itemIndex
+            if (left && left > this.items[itemIndex]) {
+                indexOfLargestItem = leftIndex
+            }
+            if (right && right > this.items[indexOfLargestItem]) {
+                indexOfLargestItem = rightIndex
+            }
+
+            if (indexOfLargestItem == itemIndex) {
+                break
+            }
+
+            const item = this.items[itemIndex]
+            this.items[itemIndex] = this.items[indexOfLargestItem]
+            this.items[indexOfLargestItem] = item
+            itemIndex = indexOfLargestItem
+        }
+
+        return root
+    }
+}
+
+
 export class PriorityQueueArray {
     items: number[] = []
 
@@ -6,20 +76,40 @@ export class PriorityQueueArray {
     }
 
     insert(x: number) {
-        for(let i = 0; i < this.items.length; i++) {
-            if (x >= this.items[i]) {
-                this.items.splice(i, 0, x)
-                return
+        let insertionIndex: number | undefined = undefined
+        this.items.some((item, i): boolean => {
+            if (x >= item) {
+                insertionIndex = i
+                return true
             }
-        }
 
-        // If we got here, x was not greater than any item and must be the smallest
-        // Add it to the end of the array
-        this.items.push(x)
+            return false
+        })
+
+        if (insertionIndex) {
+            this.items.splice(insertionIndex, 0, x)
+        }
+        else {
+            // If we got here, x was not greater than any item and must be the smallest
+            // Add it to the end of the array
+            this.items.push(x)
+        }
     }
 
     removeMax() {
         return this.items.shift()
+    }
+}
+
+export class PriorityQueue {
+    heap: Heap = new Heap()
+
+    insert(x: number) {
+        this.heap.insert(x)
+    }
+
+    removeMax() {
+        return this.heap.remove()
     }
 }
 
