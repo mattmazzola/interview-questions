a4a4f7c2bfd4c152ae3716085ad0af4001357041
Matt Mazzola <mattmazzola@live.com>
2017-07-01 18:14:25 -0700

    Add more basic searching problems

--- src/hackerearth/algorithms/searching.test.ts
+++ src/hackerearth/algorithms/searching.test.ts
@@ -2,45 +2,120 @@ import * as search from './searching'
 
 describe('hackerearth', () => {
     describe('searching', () => {
-        describe('getVowelCount', () => {
-            test('given empty string should return 0', () => {
-                expect(search.getVowelCount('')).toBe(0)
+        describe('linear', () => {
+            describe('getVowelCount', () => {
+                test('given empty string should return 0', () => {
+                    expect(search.getVowelCount('')).toBe(0)
+                })
+
+                test('given string should return number of vowels', () => {
+                    expect(search.getVowelCount('xxxx')).toBe(0)
+                    expect(search.getVowelCount('abce123')).toBe(2)
+                    expect(search.getVowelCount('JHkIsnZtTL')).toBe(1)
+                })
             })
 
-            test('given string should return number of vowels', () => {
-                expect(search.getVowelCount('xxxx')).toBe(0)
-                expect(search.getVowelCount('abce123')).toBe(2)
-                expect(search.getVowelCount('JHkIsnZtTL')).toBe(1)
+            describe('getDate', () => {
+                test('given sample messages should return date wigh highest weight', () => {
+                    // Arrange
+                    const messages = [
+                        'G: I want to go on 19',
+                        'M: No that is not possible lets go on 21',
+                        'G: No 19 is final and 21 is not',
+                        'M: OKAY as you wish'
+                    ]
+
+                    // Act
+                    const date = search.getDate(messages)
+
+                    // Assert
+                    expect(date).toBe(19)
+                })
+            })
+
+            describe('minMax', () => {
+                test('given array of integers return smallest and largest sum', () => {
+                    expect(search.minMax([1,2,3,4,5])).toEqual([10,14])
+                })
+            })
+
+            describe('repeatedKTimes', () => {
+                test('given array of numbers return the minimum number which is repeated k times', () => {
+                    expect(search.repeatedKTimes([2, 2, 1, 3, 1], 2)).toBe(1)
+                })
             })
         })
 
-        describe('getDate', () => {
-            test('given sample messages should return date wigh highest weight', () => {
-                // Arrange
-                const messages = [
-                    'G: I want to go on 19',
-                    'M: No that is not possible lets go on 21',
-                    'G: No 19 is final and 21 is not',
-                    'M: OKAY as you wish'
-                ]
-
-                // Act
-                const date = search.getDate(messages)
-
-                // Assert
-                expect(date).toBe(19)
+        describe('binary', () => {
+            describe('getKillCountAndSum', () => {
+                test('given N soliders and power of M should kill solidiers with powe less than or equal to M', () => {
+                    expect(search.getKillCountAndSum([1,2,3,4,5,6,7], 3)).toEqual([3,6])
+                    expect(search.getKillCountAndSum([1,2,3,4,5,6,7], 10)).toEqual([7, 28])
+                    expect(search.getKillCountAndSum([1,2,3,4,5,6,7], 2)).toEqual([2, 3])
+                })
             })
-        })
 
-        describe('minMax', () => {
-            test('given array of integers return smallest and largest sum', () => {
-                expect(search.minMax([1,2,3,4,5])).toEqual([10,14])
+            describe('getUniqueTriangles', () => {
+                test('given list of triangle sides return the number of unique traingles', () => {
+                    // Arrange
+                    const triangles = [
+                        [7,6,5],
+                        [5,7,6],
+                        [8,2,9],
+                        [2,3,4],
+                        [2,4,3]
+                    ]
+
+                    expect(search.getUniqueTriangles(triangles)).toBe(1)
+                })
             })
-        })
 
-        describe('repeatedKTimes', () => {
-            test('given array of numbers return the minimum number which is repeated k times', () => {
-                expect(search.repeatedKTimes([2, 2, 1, 3, 1], 2)).toBe(1)
+            describe('stringMonster', () => {
+                // Technically this is the wrong solution since you are not allowed to use subset of the strings, it must be exact match
+                test('given a set of strings is there enough characters in those strings to create the sleep string', () => {
+                    // Arrange
+                    const strings = [
+                        'hey',
+                        'rain',
+                        'day',
+                        'wet'
+                    ]
+
+                    const strings2 = [
+                        'bump',
+                        'ud',
+                        'bish',
+                        'chau'
+                    ]
+
+                    expect(search.stringMonster(strings, 'draaxiny')).toBe(false)
+                    expect(search.stringMonster(strings2, 'bichhusa')).toBe(true)
+                })
+            })
+
+            describe('getCountOfSmaller', () => {
+                test('given array of numbers and value, return the count of numbers smaller than the value', () => {
+                    expect(search.getCountOfSmaller([], 20)).toBe(0)
+                    expect(search.getCountOfSmaller([3,4,5,7,8,9,123], 2)).toBe(0)
+                    expect(search.getCountOfSmaller([3,4,5,7,8,9,123], 500)).toBe(7)
+                    expect(search.getCountOfSmaller([1,3,4,5,7,8,9,123], 6)).toBe(4)
+                    expect(search.getCountOfSmaller([1,3,4,5,7,8,9,123], 7)).toBe(4)
+                })
+            })
+
+            describe('getCountOfLarger', () => {
+                test('given array of number and value, return the count of numbers greater than the given value', () => {
+                    expect(search.getCountOfLarger([], 10)).toBe(0)
+                    expect(search.getCountOfLarger([5,6,7,8,9,12,15,16], 10)).toBe(3)
+                    expect(search.getCountOfLarger([5,6,7,8,9,12,13,14,15,16], 9)).toBe(5)
+                    expect(search.getCountOfLarger([5,6,7,8,9,12,13,14,15,16], 10)).toBe(5)
+                })
+            })
+
+            xdescribe('simpleGame', () => {
+                test('given different numbers foreach monk return the winning monk and the score or tie', () => {
+                    expect(search.simpleGame([1,3], [0,5])).toEqual(`Monk 2`)
+                })
             })
         })
     })
--- src/hackerearth/algorithms/searching.ts
+++ src/hackerearth/algorithms/searching.ts
@@ -108,3 +108,252 @@ export const repeatedKTimes = (numbers: number[], requiredRepetitions: number): 
         
     return (sortedNumbers.length > 0) ? sortedNumbers[0] : undefined
 }
+
+/**
+ * Bishu and Soldiers
+ * https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/bishu-and-soldiers/
+ */
+export const getKillCountAndSum = (soldiersPowers: number[], bishuPower: number) => {
+    const sortedSoliderPowers = [...soldiersPowers].sort((a, b) => a - b)
+    const lastIndexOfPowersLessThanPower = findLastIndexLessThanPower(sortedSoliderPowers, bishuPower)!
+    const powersLessThanBishus = sortedSoliderPowers.filter((x, i) => i <= lastIndexOfPowersLessThanPower)
+    const sumOfPowers = powersLessThanBishus.reduce((a, b) => a + b)
+
+    return [powersLessThanBishus.length, sumOfPowers]
+}
+
+const findLastIndexLessThanPowerLinear = (numbers: number[], x: number): number => {
+    let lastIndex = -1
+
+    numbers.some((n, i) => {
+        if (n > x) {
+            return true
+        }
+
+        lastIndex = i
+        return false
+    })
+
+    return lastIndex
+}
+
+const findLastIndexLessThanPower = (numbers: number[], number: number, minIndex: number = 0, maxIndex: number = numbers.length - 1): number | undefined => {
+    if (numbers.length == 0) {
+        return undefined
+    }
+
+    if (minIndex >= maxIndex) {
+        return minIndex
+    }
+
+    const midIndex = Math.floor((minIndex + maxIndex) / 2)
+    const mid = numbers[midIndex]
+
+    if (mid === number) {
+        return midIndex
+    }
+
+    if (number < mid) {
+        return findLastIndexLessThanPower(numbers, number, minIndex, midIndex - 1)
+    }
+
+    if (number > mid) {
+        return findLastIndexLessThanPower(numbers, number, midIndex + 1, maxIndex)
+    }
+}
+
+/**
+ * Counting Triangles
+ * https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/counting-triangles/
+ */
+export const getUniqueTriangles = (triangles: number[][]): number => {
+    const triangleFrequency = triangles
+        .map(traingleSides => [...traingleSides].sort((a, b) => a - b).join(''))
+        .reduce((uniqueTriangles: any, triangleSidesString) => {
+            if (uniqueTriangles[triangleSidesString]) {
+                uniqueTriangles[triangleSidesString] += 1
+            }
+            else {
+                uniqueTriangles[triangleSidesString] = 1
+            }
+
+            return uniqueTriangles
+        }, {})
+
+    return Object.keys(triangleFrequency)
+        .filter(key => triangleFrequency[key] === 1)
+        .length
+}
+
+/**
+ * The String Monster
+ * https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/the-string-monster-july-easy/
+ */
+export const stringMonster = (availableStrings: string[], sleepString: string): boolean => {
+    const characterFrequencies = availableStrings
+        .map(s => getCharacterFrequency(s))
+        .reduce((mergedFrequency, frequency) => {
+            Object.keys(frequency)
+                .forEach(key => {
+                    if (mergedFrequency[key]) {
+                        mergedFrequency[key] += frequency[key]
+                    }
+                    else {
+                        mergedFrequency[key] = frequency[key]
+                    }
+                })
+
+            return mergedFrequency
+        }, {})
+
+    const sleepCharacterFrequencies = convertFrequenceyToArray(getCharacterFrequency(sleepString))
+    return sleepCharacterFrequencies
+        .every(frequencyElement => frequencyElement.frequency <= characterFrequencies[frequencyElement.value])
+}
+
+const getCharacterFrequency = (s: string): { [x: string]: number } => {
+    return s.split('').reduce((frequency: any, character) => {
+        if (frequency[character]) {
+            frequency[character] += 1
+        }
+        else {
+            frequency[character] = 1
+        }
+
+        return frequency
+    }, {})
+}
+
+interface IFrequencySet {
+    [x: string]: number
+}
+
+interface IFrequencyElement {
+    value: string,
+    frequency: number
+}
+
+const convertFrequenceyToArray = (frequencySet: IFrequencySet): IFrequencyElement[] => {
+    return Object.keys(frequencySet)
+        .map(key =>
+            ({
+                value: key,
+                frequency: frequencySet[key]
+            }))
+}
+
+/**
+ * Simple Game
+ * https://www.hackerearth.com/practice/algorithms/searching/binary-search/practice-problems/algorithm/simple-game-22/
+ */
+export const simpleGame = (monkASet: number[], monkBSet: number[]): string => {
+    const sortedMonkASet = [...monkASet].sort((a, b) => a - b)
+    const sortedMonkBSet = [...monkBSet].sort((a, b) => a - b)
+
+    const monkAScore = getScore(sortedMonkASet, sortedMonkBSet)
+    const monkBScore = getScore(sortedMonkBSet, sortedMonkASet)
+
+    if (monkAScore > monkBScore) {
+        return `Monk ${monkAScore}`
+    }
+    else if (monkBScore > monkBScore) {
+        return `!Monk ${monkBScore}`
+    }
+    else {
+        return `Tie`
+    }
+}
+
+const getScore = (as: number[], bs: number[]): number => {
+    return as
+        .reduce((score, value) => {
+            score += getV(bs, value)
+            return score
+        })
+}
+
+const getV = (numbers: number[], x: number): number => {
+    return getCountOfSmaller(numbers, x) * getCountOfLarger(numbers, x)
+}
+
+export const getCountOfSmaller = (numbers: number[], x: number): number => {
+    if (numbers.length === 0) {
+        return 0
+    }
+
+    if (x < numbers[0]) {
+        return 0
+    }
+
+    if (x >= numbers[numbers.length - 1]) {
+        return numbers.length
+    }
+
+    return getCountOfSmallerSearch(numbers, x)!
+}
+
+const getCountOfSmallerSearch = (numbers: number[], number: number, minIndex: number = 0, maxIndex: number = numbers.length - 1): number | undefined => {
+    if (numbers.length == 0) {
+        return undefined
+    }
+
+    if (minIndex >= maxIndex) {
+        return minIndex
+    }
+
+    const midIndex = Math.floor((minIndex + maxIndex) / 2)
+    const mid = numbers[midIndex]
+
+    if (mid === number) {
+        return midIndex - 1
+    }
+
+    if (number < mid) {
+        return getCountOfSmallerSearch(numbers, number, minIndex, midIndex - 1)
+    }
+
+    if (number > mid) {
+        return getCountOfSmallerSearch(numbers, number, midIndex + 1, maxIndex)
+    }
+}
+
+export const getCountOfLarger = (numbers: number[], x: number): number => {
+    if (numbers.length === 0) {
+        return 0
+    }
+
+    if (x < numbers[0]) {
+        return numbers.length
+    }
+
+    if (x >= numbers[numbers.length - 1]) {
+        return 0
+    }
+
+    return numbers.length - 1 - getCountOfLargerSearch(numbers, x)!
+}
+
+const getCountOfLargerSearch = (numbers: number[], number: number, minIndex: number = 0, maxIndex: number = numbers.length - 1): number | undefined => {
+    if (numbers.length == 0) {
+        return undefined
+    }
+
+    if (minIndex >= maxIndex) {
+        return maxIndex
+    }
+
+    const midIndex = Math.floor((minIndex + maxIndex) / 2)
+    const mid = numbers[midIndex]
+
+    if (mid === number) {
+        return midIndex
+    }
+
+    if (number < mid) {
+        return getCountOfLargerSearch(numbers, number, minIndex, midIndex - 1)
+    }
+
+    if (number > mid) {
+        return getCountOfLargerSearch(numbers, number, midIndex + 1, maxIndex)
+    }
+}
