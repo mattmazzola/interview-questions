93d1693deff3817963efd2065051dae2630c0f8d
Matt Mazzola <mattmazzola@live.com>
2017-07-09 16:07:08 -0700

    Finish up all Trees and Graphs problems except 4.8

--- src/crackingthecodinginterview/treesandgraphs.test.ts
+++ src/crackingthecodinginterview/treesandgraphs.test.ts
@@ -124,7 +124,7 @@ describe('Cracking The Coding Interview', () => {
 
         describe('Get all paths in graph', () => {
             test('given graph should return list of paths which reach leaves or a visited node', () => {
-                expect(TreesAndGraphs.getPaths(graph, x => x)).toEqual([
+                expect(TreesAndGraphs.getPaths(graph)).toEqual([
                     [1,2,4],
                     [1,2,5],
                     [1,3,6],
@@ -182,6 +182,286 @@ describe('Cracking The Coding Interview', () => {
                 // Assert
                 expect(bst).toEqual(expected)
             })
+        })
+
+        describe('4.4 Given binary tree return linked list for all the nodes at each depth', () => {
+            test('given null return empty array', () => {
+                expect(TreesAndGraphs.convertBinaryTreeToLinkedLists(null!)).toEqual([])
+            })
+
+            test('given binary tree with one node should return single node', () => {
+                // Arange
+                const node: TreesAndGraphs.IBinaryNode<number> = { value: 1, left: null, right: null }
+                const expected = [[1]]
+
+                // Act/Assert
+                expect(TreesAndGraphs.convertBinaryTreeToLinkedLists(node)).toEqual(expected)
+            })
+
+            test('given binary tree return lists for each level of tree', () => {
+                // Arrange
+                const tree: TreesAndGraphs.IBinaryNode<number> = {
+                    value: 1,
+                    left: {
+                        value: 2,
+                        left: {
+                            value: 10,
+                            left: node(3),
+                            right: null
+                        },
+                        right: null
+                    },
+                    right: {
+                        value: 5,
+                        left: node(6),
+                        right: node(7)
+                    }
+                }
+
+                const expected = [
+                    [1],
+                    [2,5],
+                    [10,6,7],
+                    [3]
+                ]
+
+                // Act
+                const actual = TreesAndGraphs.convertBinaryTreeToLinkedLists(tree)
+
+                // Assert
+                expect(actual).toEqual(expected)
+            })
+        })
+
+        describe('4.5 Given binary tree determine if it is binary search tree', () => {
+            test('given binary search tree return true', () => {
+                // Arrange
+                const tree: TreesAndGraphs.IBinaryNode<number> = {
+                    value: 4,
+                    left: {
+                        value: 2,
+                        left: null,
+                        right: null
+                    },
+                    right: {
+                        value: 6,
+                        left: null,
+                        right: null
+                    }
+                }
+
+                // Act/Assert
+                expect(TreesAndGraphs.isBinaryTreeBinarySearchTree(tree)).toBe(true)
+            })
+
+            test('given binary tree return false', () => {
+                // Arrange
+                const tree: TreesAndGraphs.IBinaryNode<number> = {
+                    value: 1,
+                    left: {
+                        value: 2,
+                        left: null,
+                        right: null
+                    },
+                    right: {
+                        value: 6,
+                        left: null,
+                        right: null
+                    }
+                }
+
+                // Act/Assert
+                expect(TreesAndGraphs.isBinaryTreeBinarySearchTree(tree)).toBe(false)
+            })
+        })
+
+        describe('4.6 Given BST with parent nodes, return in-order successor', () => {
+            const root: TreesAndGraphs.IBinaryNodeParent<number> = {
+                parent: null,
+                value: 10,
+                left: null,
+                right: null
+            }
+
+            root.left = {
+                parent: root,
+                value: 5,
+                left: null,
+                right: null
+            }
+
+            root.right = {
+                parent: root,
+                value: 15,
+                left: null,
+                right: null
+            }
+
+            root.left.left = {
+                parent: root.left,
+                value: 2,
+                left: null,
+                right: null
+            }
+
+            root.left.right = {
+                parent: root.right,
+                value: 7,
+                left: null,
+                right: null
+            }
+
+            root.left.right.left = {
+                parent: root.left.right,
+                value: 6,
+                left: null,
+                right: null
+            }
+
+            root.right = {
+                parent: root,
+                value: 15,
+                left: null,
+                right: null
+            }
+
+            root.right.left = {
+                parent: root.right,
+                value: 12,
+                left: null,
+                right: null
+            }
+
+            root.right.left.right = {
+                parent: root.right.left,
+                value: 13,
+                left: null,
+                right: null
+            }
+
+            root.right.right = {
+                parent: root.right,
+                value: 17,
+                left: null,
+                right: null
+            }
+
+            test('given node with right children, return next larger node', () => {
+                expect(TreesAndGraphs.findInorderSuccessor(root)).toBe(root.right!.left)
+            })
+
+            test('given node with no right children and no larger parent return null', () => {
+                expect(TreesAndGraphs.findInorderSuccessor(root.right!.right!)).toBe(null)
+            })
+            
+            test('given node with no right children and greater parent return parent', () => {
+                expect(TreesAndGraphs.findInorderSuccessor(root.left!.left!)).toBe(root.left!)
+            })
+
+            test('given node with no right children and smaller parent return next largest node', () => {
+                expect(TreesAndGraphs.findInorderSuccessor(root.right!.left!.right!)).toBe(root.right!)
+            })
+        })
+
+        describe('4.7 Given two nodes, return first common ancestor (slow naive version)', () => {
+            test('given two nodes that are not in the same tree return null', () => {
+                // Arrange
+                const a: TreesAndGraphs.IBinaryNodeParent<number> = {
+                    value: 1,
+                    parent: null,
+                    left: null,
+                    right: null
+                }
+
+                const b: TreesAndGraphs.IBinaryNodeParent<number> = {
+                    value: 2,
+                    parent: null,
+                    left: null,
+                    right: null
+                }
+
+                // Act
+                const actual = TreesAndGraphs.findCommonAncestor(a, b)
+
+                // Assert
+                expect(actual).toBe(null)
+            })
+
+            test('given two nodes thare in the same tree return first common ancestor', () => {
+                // Arrange
+                const createNode = (value: number, parent: TreesAndGraphs.IBinaryNodeParent<number> | null = null): TreesAndGraphs.IBinaryNodeParent<number> =>
+                    ({
+                        parent,
+                        value,
+                        left: null,
+                        right: null
+                    })
+
+                const tree = createNode(1)
+                const left = createNode(2, tree)
+                const right = createNode(3, tree)
+                const leftleft = createNode(4, left)
+                const leftright = createNode(5, left)
+                const rightleft = createNode(6, right)
+                const rightright = createNode(7, right)
+
+                // Act
+                const firstCommonAncester = TreesAndGraphs.findCommonAncestor(leftleft, rightleft)
+
+                // Assert
+                expect(firstCommonAncester).toBe(tree)
+            })
+        })
+
+        describe('4.9 Given binary tree return paths which add to sum', () => {
+            // Arrange
+            const tree: TreesAndGraphs.IBinaryNode<number> = {
+                value: 1,
+                left: {
+                    value: 2,
+                    left: {
+                        value: 2,
+                        left: null,
+                        right: null
+                    },
+                    right: {
+                        value: 3,
+                        left: null,
+                        right: null
+                    }
+                },
+                right: {
+                    value: 3,
+                    left: {
+                        value: 2,
+                        left: null,
+                        right: null
+                    },
+                    right: {
+                        value: 1,
+                        left: {
+                            value: 1,
+                            left: null,
+                            right: null
+                        },
+                        right: null
+                    }
+                }
+            }
+
+            const nodes = TreesAndGraphs.convertBinaryTreeToTree(tree)!
+
+            const expected = [
+                [1,2,3],
+                [1,3,2],
+                [1,3,1,1]
+            ]
+
+            // Act
+            const paths = TreesAndGraphs.getPathsOfHeight(nodes, 6)
+
+            // Assert
+            expect(paths).toEqual(expected)
         })
     })
 })
--- src/crackingthecodinginterview/treesandgraphs.ts
+++ src/crackingthecodinginterview/treesandgraphs.ts
@@ -117,22 +117,46 @@ export const dft = <T>(root: INode<T>, id: (node: T) => number, visited: { [x: n
     ]
 }
 
-export const getPaths = <T>(root: INode<T>, id: (node: T) => number, visited: { [x: number]: boolean } = {}): T[][] => {
-    if (visited[id(root.value)]) {
+export const getPaths = <T>(root: INode<T>, visited: Set<INode<T>> = new Set<INode<T>>()): T[][] => {
+    if (visited.has(root)) {
         return []
     }
 
-    visited[id(root.value)] = true
+    visited.add(root)
 
     if (root.nodes.length === 0) {
         return [[root.value]]
     }
 
     return root.nodes
-            .filter(n => !visited[id(n.value)])
-            .map(n => getPaths(n, id, visited)
+            .filter(n => !visited.has(n))
+            .map(n => getPaths(n, visited)
                 .map(path => [root.value, ...path]))
             .reduce((a, b) => a.concat(b), [])
+}
+
+export const convertBinaryTreeToTree = <T>(root: IBinaryNode<T> | null): INode<T> | null => {
+    if (root === null) {
+        return null
+    }
+
+    const node: INode<T> = {
+        value: root.value,
+        nodes: []
+    }
+
+    const left = convertBinaryTreeToTree(root.left)
+    const right = convertBinaryTreeToTree(root.right)
+
+    if (left !== null) {
+        node.nodes.push(left)
+    }
+
+    if (right !== null) {
+        node.nodes.push(right)
+    }
+
+    return node
 }
 
 /**
@@ -163,4 +187,148 @@ export const convertSortedArrayToBinarySearchTree = (numbers: number[]): IBinary
         left,
         right
     }
+}
+
+/**
+ * 4.4 Given binary tree convert to list of linked lists
+ */
+export interface ILinkedListNode<T> {
+    value: T
+    next: ILinkedListNode<T> | null
+}
+
+export const convertBinaryTreeToLinkedLists = <T>(root: IBinaryNode<T>, lists: T[][] = [], level: number = 0): T[][] => {
+    if (root === null) {
+        return []
+    }
+
+    // Add current node to the current level
+    if (lists[level]) {
+        lists[level].push(root.value)
+    }
+    else {
+        lists[level] = [root.value]
+    }
+
+    if (root.left) {
+        convertBinaryTreeToLinkedLists(root.left, lists, level + 1)
+    }
+
+    if (root.right) {
+        convertBinaryTreeToLinkedLists(root.right, lists, level + 1)
+    }
+
+    return lists
+}
+
+/**
+ * 4.5 Given binary tree determine if it's a binary search tree
+ */
+export const isBinaryTreeBinarySearchTree = (root: IBinaryNode<number>): boolean => {
+    if (root === null) {
+        return false
+    }
+
+    let isLeftBST = root.left === null || (root.left.value < root.value) && isBinaryTreeBinarySearchTree(root.left)
+    let isRightBST = isLeftBST && (root.right === null || (root.right.value > root.value) && isBinaryTreeBinarySearchTree(root.right))
+
+    return isLeftBST && isRightBST
+}
+
+/**
+ * 4.6 Find next node (in-order successor)
+ */
+export interface IBinaryNodeParent<T> {
+    parent: IBinaryNodeParent<T> | null
+    value: T
+    left: IBinaryNodeParent<T> | null
+    right: IBinaryNodeParent<T> | null
+}
+
+export const findInorderSuccessor = <T>(root: IBinaryNodeParent<T>): IBinaryNodeParent<T> | null => {
+    // Go right once, then go left until leaf
+    if (root.right) {
+        let node = root.right
+        while (node.left !== null) {
+            node = node.left
+        }
+
+        return node
+    }
+
+    // If at root retur null since there is no node with greater value
+    if (!root.parent) {
+        return null
+    }
+
+    // If our parent is greater than us return it since it's the next most highest
+    if (root.parent.value > root.value) {
+        return root.parent
+    }
+
+    // Go up tree until parent which has greater value
+    let parent = root.parent
+    while (parent.value < root.value) {
+
+        // If we reach the root before we found parent that is greater return null
+        if (parent.parent === null) {
+            return null
+        }
+
+        parent = parent.parent
+    }
+
+    // At this point the parent value is greater than root.value
+    return parent
+}
+
+/**
+ * 4.7 Find common ancestor
+ */
+export const findCommonAncestor = <T>(nodeA: IBinaryNodeParent<T>, nodeB: IBinaryNodeParent<T>): IBinaryNodeParent<T> | null => {
+    const nodeAparents = getParents(nodeA)
+    const nodeBparents = getParents(nodeB)
+
+    let firstCommonAncestor: IBinaryNodeParent<T> | null = null
+    nodeAparents.some(node => {
+        if (nodeBparents.includes(node)) {
+            firstCommonAncestor = node
+            return true
+        }
+
+        return false
+    })
+
+    return firstCommonAncestor
+}
+
+const getParents = <T>(node: IBinaryNodeParent<T>): IBinaryNodeParent<T>[] => {
+    const parents = []
+    let parent = node.parent
+    while (parent !== null) {
+        parents.push(parent)
+        parent = parent.parent
+    }
+
+    return parents
+}
+
+/**
+ * 4.9 Find paths of height
+ */
+export const getPathsOfHeight = (tree: INode<number>, height: number): number[][] => {
+    return <number[][]>getPaths(tree)
+        .map(path => {
+            let sum = 0
+            let pathSubset: number[] = []
+
+            const pathMatch = path.some(value => {
+                sum += value
+                pathSubset.push(value)
+                return sum === height
+            })
+
+            return pathMatch ? pathSubset : null
+        })
+        .filter(x => x)
 }
