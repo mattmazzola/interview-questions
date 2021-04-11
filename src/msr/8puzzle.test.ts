0d0fd15cbb18fc0008e90c6e96869e1dd3fbef28
Matt Mazzola <mattmazzola@live.com>
2021-04-11 01:21:11 -0700

    fix 8 puzzle to compile

--- src/msr/8puzzle.test.ts
+++ src/msr/8puzzle.test.ts
@@ -1,20 +1,47 @@
-import { isSolveable } from './8puzzle'
+import { isSolvable, getMovesToSolve } from './8puzzle'
 
 describe('MSR', () => {
     describe('8 Puzzle', () => {
-        test('given puzzle return if it is solvable', () => {
-            // Arrange
-            const puzzle = [
-                [1,2,3],
-                [4,5,6],
-                [8,7,0]
-            ]
-            const expected = false
+        describe('isSolvable', () => {
+            test('given solved puzzle return true', () => {
+                // Arrange
+                const puzzle = [
+                    [1,2,3],
+                    [4,5,6],
+                    [7,8,0]
+                ]
+                const expected = true
+                
+                // Act
+                const actualIsSolvable = isSolvable(puzzle, puzzle)
+                
+                // Assert
+                expect(actualIsSolvable).toBe(expected)
+            })
+        })
 
-            // Act
-            const isSolvable = isSolveable(puzzle)
-
-            // Assert
+        describe('getMoves', () => {
+            test('given puzzle return the moves to solve it', () => {
+                // Arrange
+                const puzzle = [
+                    [1,2,3],
+                    [4,5,6],
+                    [8,7,0]
+                ]
+                const expectedMoves = [
+                    {
+                        row: 1,
+                        col: 1,
+                        dir: 'down'
+                    }
+                ]
+                
+                // Act
+                const moves = getMovesToSolve(puzzle, puzzle)
+                
+                // Assert
+                expect(moves).toEqual(expectedMoves)
+            })
         })
     })
 })
--- src/msr/8puzzle.ts
+++ src/msr/8puzzle.ts
@@ -1,21 +1,116 @@
+type Puzzle = number[][]
+
 /**
  * Given a 3x3 matrix of numbers with 1 square empty leaving 8 filled squares return if the given puzzle is solvable.
  */
-export function isSolveable(puzzle: number[][]): boolean {
+export function isSolvable(puzzle: Puzzle, goalPuzzle: Puzzle): boolean {
+    // Return false if puzzle or goal isn't valid or they don't match
+    // Get maxRowIndex and maxColIndex
+    const maxRowIndex = puzzle.length - 1
+    if (maxRowIndex < 1) {
+        return false
+    }
+
+    const maxColIndex = puzzle[0].length - 1
+    if (maxColIndex < 1) {
+        return false
+    }
+
+    const inversionCount = getInversionCount(puzzle, goalPuzzle, maxRowIndex, maxColIndex)
+    if (inversionCount % 2 === 0) {
+        return true
+    }
+
     return false
+}
+
+function getInversionCount(puzzle: Puzzle, goalPuzzle: Puzzle, maxRowIndex: number, maxColIndex: number): number {
+    let inversionCount = 0
+
+    for (let i = 0; i <= maxRowIndex; i++) {
+        for (let j = 0; j < maxColIndex; j++) {
+            const puzzleNumber = puzzle[i][j]
+            const goalNumber = goalPuzzle[i][j]
+
+            if (puzzleNumber !== goalNumber) {
+                inversionCount += 1
+            }
+        }
+    }
+
+    return inversionCount
+}
+
+function isMatchingGoal(puzzle: Puzzle, goalPuzzle: Puzzle, maxRowIndex: number, maxColIndex: number): boolean {
+    const count = getInversionCount(puzzle, goalPuzzle, maxRowIndex, maxColIndex)
+
+    return count == 0
 }
 
 /**
  * Given a 3x3 matrix above, return the moves to solve the puzzle
  */
 
-type Direction = "up" | "down" | "left" | "right"
-type Move = {
-    row: number
-    col: number
-    dir: Direction
+
+export function getMovesToSolve(puzzle: Puzzle, goalPuzzle: Puzzle): Puzzle[] | undefined {
+    // Return false if puzzle or goal isn't valid or they don't match
+    // Get maxRowIndex and maxColIndex
+    const maxRowIndex = puzzle.length - 1
+    if (maxRowIndex < 1) {
+        return undefined
+    }
+
+    const maxColIndex = puzzle[0].length - 1
+    if (maxColIndex < 1) {
+        return undefined
+    }
+
+    const puzzleInstance = new PuzzleNode(puzzle, goalPuzzle)
+
+    return []
 }
 
-export function movesToSolve(puzzle: number[][]): Move[] {
-    return []
+
+class PuzzleNode {
+    private puzzle: Puzzle
+    private goalPuzzle: Puzzle
+    private maxRowIndex: number
+    private maxColIndex: number
+    private children: PuzzleNode[] = []
+    
+    constructor (puzzle: Puzzle, goalPuzzle: Puzzle) {
+        this.puzzle = puzzle;
+        this.goalPuzzle = goalPuzzle;
+        this.maxRowIndex = puzzle.length - 1
+        this.maxColIndex = puzzle[0].length - 1
+    }
+
+    isSolved(): boolean {
+        return isMatchingGoal(this.puzzle, this.goalPuzzle, this.maxRowIndex, this.maxColIndex)
+    }
+
+    expandNode(): PuzzleNode[] {
+        // move empty space up, down, left, right
+        // add each new PuzzleNode to children
+        const expandedPuzzles: PuzzleNode[] = []
+
+        return expandedPuzzles
+    }
+
+    findSolution(): PuzzleNode | undefined {
+        if (this.isSolved()) {
+            return this
+        }
+
+        const children = this.expandNode()
+        this.children.push(...children)
+
+        for(const child of this.children) {
+            // findSolution()
+        }
+
+        // const a = new PuzzleNode()
+        // return a
+    }
+
 }
