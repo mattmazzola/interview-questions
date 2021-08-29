5268e1ac5a7cf64f29013312ba5cd51d34752fa1
Matt Mazzola <mattmazzola@live.com>
2021-08-29 11:40:23 -0700

    Add connected components test

---
+++ src/graphs/connectedComoonents.test.ts
@@ -0,0 +1,60 @@
+import { Graph } from './models'
+import { getGraphComponents } from './connectedComponents'
+
+describe('Get Connected Components', () => {
+    describe('getGraphComponents', () => {
+        const graph: Graph = {
+            rootNodeId: '1',
+            nodes: [
+                {
+                    id: '1',
+                    value: 1,
+                    routes: ['2'],
+                },
+                {
+                    id: '2',
+                    value: 2,
+                    routes: ['3'],
+                },
+                {
+                    id: '3',
+                    value: 3,
+                    routes: ['1'],
+                },
+                {
+                    id: '4',
+                    value: 4,
+                    routes: ['5'],
+                },
+                {
+                    id: '5',
+                    value: 5,
+                    routes: ['6'],
+                },
+                {
+                    id: '6',
+                    value: 6,
+                    routes: ['4'],
+                },
+            ]
+        }
+
+        test('given graph with 2 components return expected count and node component ids', () => {
+            const expected = {
+                count: 2,
+                nodeColors: [
+                    ['1', 0],
+                    ['2', 0],
+                    ['3', 0],
+                    ['4', 1],
+                    ['5', 1],
+                    ['6', 1],
+                ]
+            }
+
+            const actual = getGraphComponents(graph)
+
+            expect(actual).toEqual(expected)
+        })
+    })
+})
---
+++ src/graphs/connectedComponents.ts
@@ -0,0 +1,47 @@
+import { Graph, Node } from './models'
+
+export function getGraphComponents(graph: Graph): { count: number, nodeColors: [string, number][] } {
+    let count = 0
+    let nodeColors: [string, number][] = []
+    const visited = new Set<string>()
+
+    const colorFn = (node: Node) => {
+        nodeColors.push([node.id, count])
+    }
+
+    for (const node of graph.nodes) {
+        if (!visited.has(node.id)) {
+            depthFirstTraversal(graph, node.id, visited, colorFn)
+            count += 1
+        }
+    }
+
+    return {
+        count,
+        nodeColors
+    }
+}
+
+const depthFirstAdd = (stack: string[], newIds: string[]) => stack.unshift(...newIds)
+
+export function depthFirstTraversal(
+    graph: Graph,
+    nodeId: string,
+    visited = new Set<string>(),
+    fn: (n: Node) => void
+) {
+    const queue = [nodeId]
+
+    while (queue.length > 0) {
+        const nodeId = queue.shift()!
+        const node = graph.nodes.find(n => n.id === nodeId)!
+
+        if (!visited.has(node.id)) {
+            visited.add(nodeId)
+            fn(node)
+
+            const nonVisitedNodeIds = (node.routes ?? []).filter(r => !visited.has(r))
+            depthFirstAdd(queue, nonVisitedNodeIds)
+        }
+    }
+}
