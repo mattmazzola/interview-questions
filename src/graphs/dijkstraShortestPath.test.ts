65ec479d23550210e603bfc99eee621b07672bef
Matt Mazzola <mattmazzola@live.com>
2021-09-05 00:38:09 -0700

    More dijkstraShortestPath

--- src/graphs/dijkstraShortestPath.test.ts
+++ src/graphs/dijkstraShortestPath.test.ts
@@ -1,117 +1,173 @@
 import { Graph } from './models'
-import { dijkstraShortedPath, WeightedEdge } from "./dijkstraShortestPath"
-import exp from 'constants'
+import { dijkstraShortedPathTopologicalSort, dijkstraShortedPathLazy, WeightedEdge, DistanceMap } from "./dijkstraShortestPath"
 
 describe('dijkstrasShortedPath', () => {
-    test('given graph return the shorted path from A to B', () => {
-        const graph: Graph<unknown, WeightedEdge> = {
-            rootNodeId: 'a',
-            nodes: [
-                {
-                    id: 'a',
-                    value: 1,
-                    routes: [
-                        {
-                            to: 'b',
-                            distance: 3
-                        },
-                        {
-                            to: 'c',
-                            distance: 6
-                        }
-                    ]
-                },
-                {
-                    id: 'b',
-                    value: 1,
-                    routes: [
-                        {
-                            to: 'c',
-                            distance: 4
-                        },
-                        {
-                            to: 'd',
-                            distance: 4
-                        },
-                        {
-                            to: 'e',
-                            distance: 11
-                        }
-                    ]
-                },
-                {
-                    id: 'c',
-                    value: 1,
-                    routes: [
-                        {
-                            to: 'd',
-                            distance: 8
-                        },
-                        {
-                            to: 'g',
-                            distance: 11
-                        }
-                    ]
-                },
-                {
-                    id: 'd',
-                    value: 1,
-                    routes: [
-                        {
-                            to: 'e',
-                            distance: -4
-                        },
-                        {
-                            to: 'f',
-                            distance: 5
-                        },
-                        {
-                            to: 'g',
-                            distance: 2
-                        }
-                    ]
-                },
-                {
-                    id: 'e',
-                    value: 1,
-                    routes: [
-                        {
-                            to: 'h',
-                            distance: 9
-                        }
-                    ]
-                },
-                {
-                    id: 'f',
-                    value: 1,
-                    routes: [
-                        {
-                            to: 'h',
-                            distance: 1
-                        }
-                    ]
-                },
-                {
-                    id: 'g',
-                    value: 1,
-                    routes: [
-                        {
-                            to: 'h',
-                            distance: 2
-                        }
-                    ]
-                },
-                {
-                    id: 'h',
-                    value: 1,
-                    routes: []
-                }
-            ]
-        }
+    const graph: Graph<unknown, WeightedEdge> = {
+        rootNodeId: 'a',
+        nodes: [
+            {
+                id: 'a',
+                value: 1,
+                routes: [
+                    {
+                        to: 'b',
+                        distance: 3
+                    },
+                    {
+                        to: 'c',
+                        distance: 6
+                    }
+                ]
+            },
+            {
+                id: 'b',
+                value: 1,
+                routes: [
+                    {
+                        to: 'c',
+                        distance: 4
+                    },
+                    {
+                        to: 'd',
+                        distance: 4
+                    },
+                    {
+                        to: 'e',
+                        distance: 11
+                    }
+                ]
+            },
+            {
+                id: 'c',
+                value: 1,
+                routes: [
+                    {
+                        to: 'd',
+                        distance: 8
+                    },
+                    {
+                        to: 'g',
+                        distance: 11
+                    }
+                ]
+            },
+            {
+                id: 'd',
+                value: 1,
+                routes: [
+                    {
+                        to: 'e',
+                        distance: -4
+                    },
+                    {
+                        to: 'f',
+                        distance: 5
+                    },
+                    {
+                        to: 'g',
+                        distance: 2
+                    }
+                ]
+            },
+            {
+                id: 'e',
+                value: 1,
+                routes: [
+                    {
+                        to: 'h',
+                        distance: 9
+                    }
+                ]
+            },
+            {
+                id: 'f',
+                value: 1,
+                routes: [
+                    {
+                        to: 'h',
+                        distance: 1
+                    }
+                ]
+            },
+            {
+                id: 'g',
+                value: 1,
+                routes: [
+                    {
+                        to: 'h',
+                        distance: 2
+                    }
+                ]
+            },
+            {
+                id: 'h',
+                value: 1,
+                routes: []
+            }
+        ]
+    }
 
-        const expectedDistance = 11
-        const actualDistance = dijkstraShortedPath(graph, 'a', 'h')
+    const expectedDistanceMap: DistanceMap = {
+        'a': 0,
+        'b': 3,
+        'c': 6,
+        'd': 7,
+        'e': 3,
+        'f': 12,
+        'g': 9,
+        'h': 11,
+    }
 
-        expect(actualDistance).toEqual(expectedDistance)
+    const graph2: Graph<unknown, WeightedEdge> = {
+        rootNodeId: '0',
+        nodes: [
+            {
+                id: '0',
+                value: 1,
+                routes: [{ to: '1', distance: 4 }, { to: '2', distance: 1 }]
+            },
+            {
+                id: '1',
+                value: 1,
+                routes: [{ to: '3', distance: 1 }]
+            },
+            {
+                id: '2',
+                value: 1,
+                routes: [{ to: '1', distance: 2 }, { to: '3', distance: 5 }]
+            },
+            {
+                id: '3',
+                value: 1,
+                routes: [{ to: '4', distance: 3 }]
+            },
+            {
+                id: '4',
+                value: 1,
+                routes: []
+            },
+        ]
+    }
+
+    const expectedDistanceMap2: DistanceMap = {
+        '0': 0,
+        '1': 3,
+        '2': 1,
+        '3': 4,
+        '4': 7,
+    }
+
+    test('given graph return the shorted path from A to H (topsort)', () => {
+        const nodeDistanceMap = dijkstraShortedPathTopologicalSort(graph, 'a')
+        expect(nodeDistanceMap).toEqual(expectedDistanceMap)
+        const nodeDistanceMap2 = dijkstraShortedPathTopologicalSort(graph2, '0')
+        expect(nodeDistanceMap2).toEqual(expectedDistanceMap2)
+    })
+
+    test('given graph return the shortest path from A to H (lazy)', () => {
+        const nodeDistanceMap = dijkstraShortedPathLazy(graph, 'a')
+        expect(nodeDistanceMap).toEqual(expectedDistanceMap)
+        const nodeDistanceMap2 = dijkstraShortedPathLazy(graph2, '0')
+        expect(nodeDistanceMap2).toEqual(expectedDistanceMap2)
     })
 })
--- src/graphs/dijkstraShortestPath.ts
+++ src/graphs/dijkstraShortestPath.ts
@@ -1,6 +1,6 @@
-import { Edge, Graph } from "./models";
-import { topologicalSort } from "./topologicalSort";
-import debug from 'debug';
+import { Edge, Graph } from "./models"
+import { topologicalSort } from "./topologicalSort"
+import debug from 'debug'
 
 const logger = debug('graphs:dijkstra')
 
@@ -8,35 +8,79 @@ export type WeightedEdge = Edge & {
     distance: number
 }
 
+export type DistanceMap = { [key: string]: number }
+
 // Problem from: https://youtu.be/09_LlHjoEiY?t=4415
 
-export function dijkstraShortedPath<T = unknown>(graph: Graph<T, WeightedEdge>, startNodeId: string, targetNodeId: string): number {
+export function dijkstraShortedPathTopologicalSort(graph: Graph<unknown, WeightedEdge>, startNodeId: string): DistanceMap {
     const sortedNodeIds = topologicalSort(graph)
     logger(`Sorted node ids: ${sortedNodeIds.join(', ')}`)
 
     const startNodeIndex = sortedNodeIds.findIndex(nId => nId === startNodeId)
     const nodeIdsToProcess = sortedNodeIds.slice(startNodeIndex)
-    const vertexDistanceMap = sortedNodeIds.reduce<{ [key: string]: number }>((map, nodeId) => {
-        let distance = Number.POSITIVE_INFINITY;
-        if (nodeId === startNodeId) {
-            distance = 0
-        }
-
-        map[nodeId] = distance
+    // Initialize every node to have distance of INFINITY
+    const vertexDistanceMap = sortedNodeIds.reduce<DistanceMap>((map, nodeId) => {
+        map[nodeId] = Number.POSITIVE_INFINITY
         return map
     }, {})
+    // Set start node to have distance of 0 since we are starting there
+    vertexDistanceMap[startNodeId] = 0
 
     logger(`Nodes to process: ${nodeIdsToProcess.join(', ')}`)
     logger(`Vertex Distances: ${JSON.stringify(vertexDistanceMap)}`)
 
-    for(const nodeId of nodeIdsToProcess) {
+    for (const nodeId of nodeIdsToProcess) {
         const node = graph.nodes.find(n => n.id === nodeId)!
         for (const edge of node.routes ?? []) {
             const minDistance = Math.min(vertexDistanceMap[nodeId] + edge.distance, vertexDistanceMap[edge.to])
             logger(`parent: ${nodeId} to: ${edge.to} min: ${minDistance}`)
             vertexDistanceMap[edge.to] = minDistance
+            logger(`Vertex Distances: ${JSON.stringify(vertexDistanceMap)}`)
         }
     }
 
-    return vertexDistanceMap[targetNodeId]
+    logger(`Final Vertex Distances: ${JSON.stringify(vertexDistanceMap)}`)
+
+    return vertexDistanceMap
+}
+
+// https://youtu.be/09_LlHjoEiY?t=5242
+
+export function dijkstraShortedPathLazy(graph: Graph<unknown, WeightedEdge>, startNodeId: string) {
+    const vertexDistanceMap = graph.nodes.reduce<DistanceMap>((map, node) => {
+        map[node.id] = Number.POSITIVE_INFINITY
+        return map
+    }, {})
+    vertexDistanceMap[startNodeId] = 0
+
+    const priorityQueue: [string, number][] = [[startNodeId, 0]]
+    
+    while (priorityQueue.length > 0) {
+        logger(`DistanceMap: ${JSON.stringify(vertexDistanceMap)}`)
+        logger(`Priority Queue: [${priorityQueue}]`)
+        const [nodeId, nodeDistance] = priorityQueue.shift()!
+        if (nodeDistance > vertexDistanceMap[nodeId]) {
+            logger(`Skipping queue item. Node: ${nodeId} queue distance: ${nodeDistance} existing distance: ${vertexDistanceMap[nodeId]}`)
+            continue;
+        }
+
+        logger(`Find node: ${nodeId}`)
+        const node = graph.nodes.find(n => n.id === nodeId)!
+
+        for (const edge of node.routes ?? []) {
+            const newDistance = nodeDistance + edge.distance
+            if (newDistance < vertexDistanceMap[edge.to]) {
+                vertexDistanceMap[edge.to] = newDistance
+                priorityQueue.push([edge.to, newDistance])
+            }
+        }
+
+        // Simulate priority by sorting
+        logger(`Priority Queue PreSort: [${[...priorityQueue]}]`)
+        priorityQueue.sort(([nodeA, dA], [nodeB, dB]) => dA - dB)
+        logger(`Priority Queue PostSort: [${priorityQueue}]`)
+
+    }
+
+    return vertexDistanceMap
 }
