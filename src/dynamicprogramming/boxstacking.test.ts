c0e78b94ab2e5b0a915c1cbda826dd4e04d3434c
Matthew Mazzola <mattmazzola@live.com>
2025-02-15 13:17:30 -0800

    Box stacking

--- src/dynamicprogramming/boxstacking.test.ts
+++ src/dynamicprogramming/boxstacking.test.ts
@@ -14,7 +14,9 @@ describe('Box Stacking', () => {
 
     test('given an array with one box, should return the height of the box', () => {
         // Arrange
-        const boxes: Box[] = [[1, 2, 3]]
+        const boxes: Box[] = [
+            [1, 2, 3],
+        ]
 
         // Act
         const result = tallestStack(boxes)
@@ -26,9 +28,29 @@ describe('Box Stacking', () => {
     test('given an array with multiple boxes, should return the height of the tallest stack', () => {
         // Arrange
         const boxes: Box[] = [
+            [2, 3, 4],
+            [3, 4, 5],
+            [1, 2, 3],
+        ]
+
+        // Act
+        const result = tallestStack(boxes)
+
+        // Assert
+        expect(result).toEqual(12)
+    })
+
+
+    test('given an array with multiple stacks within set, should return the height of the tallest stack', () => {
+        // Arrange
+        const boxes: Box[] = [
             [1, 2, 3],
             [2, 3, 4],
             [3, 4, 5],
+
+            [2, 2, 1],
+            [3, 3, 1],
+            [4, 4, 1],
         ]
 
         // Act
--- src/dynamicprogramming/boxstacking.ts
+++ src/dynamicprogramming/boxstacking.ts
@@ -14,7 +14,43 @@ export function tallestStack(boxes: Box[]): number {
     }
 
     // Sort boxes by area in descending order
-    boxes.sort((a, b) => b[0] * b[1] - a[0] * a[1])
+    // boxes.sort((a, b) => b[0] * b[1] - a[0] * a[1])
 
-    return boxes[0][2]
+    // For each box, get a list of boxes that can be placed on top of it
+    const boxToStackableBoxes = new Map<Box, Box[]>()
+    for (const box of boxes) {
+        boxToStackableBoxes.set(box, [])
+
+        for (const otherBox of boxes) {
+            // Skip it not self
+            if (box === otherBox) {
+                continue
+            }
+
+            // If otherBox can be placed on top of box, add it to the list
+            const [bl, bw] = box
+            const [ol, ow] = otherBox
+            if (bl > ol && bw > ow) {
+                boxToStackableBoxes.get(box)!.push(otherBox)
+            }
+        }
+    }
+
+    // Build a map between box and the height of the tallest stack that can be built with it
+    const boxToStackHeight = new Map<Box, number>()
+    for (const box of boxes) {
+        const [,, bh] = box
+        boxToStackHeight.set(box, bh)
+    }
+
+    // For each box, find the tallest stack that could be build with other boxes that can be placed on top of it
+    for (const box of boxes) {
+        for (const stackableBox of boxToStackableBoxes.get(box)!) {
+            const [,, bh] = box
+            boxToStackHeight.set(box, Math.max(boxToStackHeight.get(box)!, boxToStackHeight.get(stackableBox)! + bh))
+        }
+    }
+
+    // Return the height of the tallest stack
+    return Math.max(...boxToStackHeight.values())
 }
